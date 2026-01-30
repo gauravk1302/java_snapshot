15e3a3968dd24527c205bbec39f9e03b53e57d6b
gauravk1302 <gauravsaini77069@gmail.com>
2026-01-30 09:59:51 +0530

    Variable and Hello program

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
+/* 
+static -> means we are creating the class not the object of the class
+
+file name must match with the class name 
+
+compile ->
+run -> class file
+
+string[] args -> command line argument
+*/
---
+++ Variable.java
@@ -0,0 +1,13 @@
+public class Variable {
+    public static void main(String[] args){
+        int a=2;
+        float b=3.45f;
+        double c=5.46;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println("The numbers are:"+a+" "+b+" "+c);
+
+    }
+}
