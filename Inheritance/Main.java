39b8b6272126745e2f093b8c648ddfd419f6963c
gauravk1302 <gauravsaini77069@gamil.com>
2026-02-11 11:10:42 +0530

    Inheritance Concept

---
+++ Inheritance/Main.java
@@ -0,0 +1,29 @@
+class Parent{
+    void display(){
+        System.out.println("I am in display() of parent class");
+        privateMethod();
+    }
+    private void privateMethod(){
+        System.out.println("I am in private method of parent class");
+    }
+}
+class Child extends Parent{
+    void show(){
+        System.out.println("I am in show () of child class");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Parent P = new Parent();
+        P.display();
+        Child C= new Child();
+        C.display();
+        C.show();
+
+        // C.privateMethod();
+        // Not Accessible
+
+        //If wanted to access call this method which is accessible by child class 
+    }
+}
