8988ad7cb7e280b291cc0c41408c590886b94164
gauravk1302 <gauravsaini77069@gamil.com>
2026-02-25 12:07:54 +0530

    Exception Handling

---
+++ Exception/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int a = S.nextInt();
+
+        int b = S.nextInt();
+        int res = a / b;
+        System.out.println("The result is : " + res);
+        S.close();
+
+    }
+
+}
